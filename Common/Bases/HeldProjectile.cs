0d154e54fc5fb83667dc724dcc3be4dfee61c0d2
flats <hiimhibiscus@gmail.com>
2024-03-08 23:27:51 -0500

    held projectile base

---
+++ Common/Bases/HeldProjectile.cs
@@ -0,0 +1,32 @@
+﻿namespace TheGrotto.Common.Bases {
+    public abstract class HeldProjectile : ModProjectile {
+
+        Player player => Main.player[Projectile.owner];
+        public Vector2 holdoutOffset;
+        public bool despawned;
+        public bool changePlayerDirection = false;
+        private int associatedItem = -1;
+
+        public override bool PreAI() {
+            player.heldProj = Projectile.whoAmI;
+
+            if(changePlayerDirection) {
+                int playerDirection = Math.Sign(Main.MouseWorld.X - player.Center.X);
+                player.ChangeDir(playerDirection);
+            }
+
+            return true;
+        }
+
+        protected bool ShouldKill() {
+            return player.noItems || player.CCed;
+        }
+
+        protected void SetPositionToPlayer() {
+            Projectile.Center = player.RotatedRelativePoint(player.MountedCenter) + holdoutOffset;
+        }
+
+        public void SetAssociatedItem(int item) => associatedItem = item;
+
+    }
+}
---
+++ Common/Bases/HeldProjectileItem.cs
@@ -0,0 +1,31 @@
+﻿namespace TheGrotto.Common.Bases {
+    public abstract class HeldProjectileItem<T> : ModItem where T : HeldProjectile {
+
+        public T thisProjectile;
+
+        public override void SetDefaults() {
+            Item.channel = true;
+            Item.noMelee = true;
+            Item.noUseGraphic = true;
+        }
+
+        public static T SpawnHeldProjectile(Player player, int type, int itemType, int dmg, float kb, int owner) {
+            Projectile proj 
+                = Projectile.NewProjectileDirect(player.GetSource_FromThis(), 
+                player.Center, Vector2.Zero, type, dmg, kb, owner);
+
+            T heldProj = proj.ModProjectile as T;
+            heldProj.SetAssociatedItem(itemType);
+
+            return heldProj;
+        }
+
+        public override void HoldItem(Player player) {
+            if(thisProjectile == null || thisProjectile.Projectile.active == false)
+                thisProjectile = SpawnHeldProjectile(player, ModContent.ProjectileType<T>(), Type, 
+                    Item.damage, Item.knockBack, player.whoAmI);
+            base.HoldItem(player);
+        }
+
+    }
+}
